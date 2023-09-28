d36bb09b5443cdfb64ea3c55ea95aa4caeb81d45
El Boukhari Youcef <ussefelboukhari@gmail.com>
2023-09-28 23:13:39 +0200

    first commit

--- src/Planet.java
+++ src/Planet.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Planet {
+
+public class Planet {
+    private static Planet instance;
+    private String name;
+
+    public Planet(String name) {
+        this.name = name;
+    }
+
+    public static Planet getInstance(String name) {
+        if (instance == null) {
+            instance = new Planet(name);
+        }
+        return instance;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/SolarSystem.java
+++ src/SolarSystem.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class SolarSystem {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolarSystem {
+    private static SolarSystem instance;
+    private String sun;
+    private List<Planet> planets;
+
+    private SolarSystem() {
+        sun = "Sun";
+        planets = new ArrayList<>();
+        // Ajoutez ici les huit planètes du système solaire
+        planets.add(new Planet("Mercury"));
+        planets.add(new Planet("Venus"));
+        planets.add(new Planet("Earth"));
+        planets.add(new Planet("Mars"));
+        planets.add(new Planet("Jupiter"));
+        planets.add(new Planet("Saturn"));
+        planets.add(new Planet("Uranus"));
+        planets.add(new Planet("Neptune"));
+    }
+
+    public static SolarSystem getInstance() {
+        if (instance == null) {
+            instance = new SolarSystem();
+        }
+        return instance;
+    }
+
+    public List<Planet> getPlanets() {
+        return planets;
+    }
+
+    public String getSun() {
+        return sun;
+    }
 }
